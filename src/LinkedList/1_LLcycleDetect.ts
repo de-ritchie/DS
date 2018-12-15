52575d9a7edff34c38a217c7803aa15964dd5177
chiraj-j <chiraj.hacks@gmail.com>
2018-12-15 20:43:39 +0530

    Linked List basics

--- src/LinkedList/1_LLcycleDetect.ts
+++ src/LinkedList/1_LLcycleDetect.ts
@@ -7,3 +7,45 @@
  *                8 <- 9
 */
 
+import { LinkedList } from './LL'
+
+let ll = new LinkedList<Number>();
+
+for(let i of [1,2,3,4,5,6,7,8]){
+    ll.add(i);
+}
+
+ll.getByIndex(7).next = ll.getByIndex(2);
+
+//ll.printAllNodes();
+console.log(detectCycleByFloyd(ll));
+
+function detectCycle(ll: LinkedList<any>) {
+    
+    let map = new Map();
+    
+    let currNode = ll.head;
+    while(currNode){
+        if(map.get(currNode)){
+            return 'Contains a loop';
+        } else{
+            map.set(currNode, 'value');
+            currNode = currNode.next;
+        }
+    }
+    return 'No loop';
+}
+
+function detectCycleByFloyd(ll: LinkedList<any>) {
+    
+    let slowPtr = ll.head, fastPtr;
+    fastPtr = slowPtr.next && slowPtr.next.next;
+
+    while(slowPtr && fastPtr && (slowPtr != fastPtr)){
+        slowPtr = slowPtr.next;
+        fastPtr = fastPtr.next && fastPtr.next.next;
+        console.log(fastPtr);
+    }
+
+    return (slowPtr && fastPtr && slowPtr==fastPtr) ? 'Contains a loop' : 'No loop';
+}
--- src/LinkedList/LL.ts
+++ src/LinkedList/LL.ts
@@ -1,6 +1,56 @@
-class LinkedList{
+export class LinkedList <T>{
+
+    head: DSNode <T>;
 
     constructor(){
+
+    }
+
+    //adds a new node at last
+    add(value: T){
         
+        let newNode = new DSNode(value);
+        if(this.head){
+            let currNode = this.head;
+            while(currNode.next){
+                currNode = currNode.next;
+            }
+            currNode.next = newNode;
+        } else{
+            this.head = newNode;
+        }
+        return newNode;
+    }
+
+    getByIndex(i: number){
+
+        let j = 0;
+        let currNode = this.head;
+
+        while(i != j && currNode.next){
+            currNode = currNode.next;
+            j++;
+        }
+        return (i == j && currNode) ? currNode : null;
+    }
+
+    printAllNodes(){
+
+        let currNode = this.head;
+        while(currNode){
+            console.log('Node value', currNode.value);
+            currNode = currNode.next;
+        }
+    }
+}
+
+class DSNode <T>{
+    
+    value: T;
+    next: DSNode <T>;
+
+    constructor(value: T){
+        this.value = value;
+        this.next = null;
     }
 }
